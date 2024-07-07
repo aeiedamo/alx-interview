3185b45ab601332089ba2a8ca5791649cc814bd8
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-07-07 16:19:40 +0100

    0x01 - add file #0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/share/python3
+"""
+  You have n number of locked boxes in front of you.
+  Each box is numbered sequentially from 0 to n - 1
+  and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+  """
+    function to check the unlock-ability of the boxes.
+  """
+  if not isinstance(boxes, list):
+    return False
+  elif len(boxes) == 0:
+    return False
+
+
+  for key in range(1, len(boxes) - 1):
+    boxes_checked = False
+
+    for i in range(len(boxes)):
+      boxes_checked = key in boxes[i] and i != key
+      if boxes_checked:
+        break
+
+    if boxes_checked is False:
+      return boxes_checked
+
+  return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,3 @@
+## 0x01. Lockboxes
+
+checkout the [PDF](./0x01.Lockboxes.pdf) for more details.
