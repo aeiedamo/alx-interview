8f13484f6e1485b1fc0dd0a3716aac9c35a2ddcb
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-09-20 17:20:31 +0100

    0x0A - add components

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,70 +1,57 @@
 #!/usr/bin/python3
 """
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number from the
-set and removing that number and its multiples from the set. The player that
-cannot make a move loses the game.
-
-They play x rounds of the game, where n may be different for each round.
-Assuming Maria always goes first and both players play optimally, determine who
-the winner of each game is.
-
-Prototype: def isWinner(x, nums)
-where x is the number of rounds and nums is an array of n
-Return: name of the player that won the most rounds
-If the winner cannot be determined, return None
-You can assume n and x will not be larger than 10000
-You cannot import any packages in this task
-Example:
-
-x = 3, nums = [4, 5, 1]
-First round: 4
-
-Maria picks 2 and removes 2, 4, leaving 1, 3
-Ben picks 3 and removes 3, leaving 1
-Ben wins because there are no prime numbers left for Maria to choose
-Second round: 5
-
-Maria picks 2 and removes 2, 4, leaving 1, 3, 5
-Ben picks 3 and removes 3, leaving 1, 5
-Maria picks 5 and removes 5, leaving 1
-Maria wins because there are no prime numbers left for Ben to choose
-Third round: 1
-
-Ben wins because there are no prime numbers for Maria to choose
-
-Result: Ben has the most wins
+details in pdf.
 """
+
 
 def isPrime(n):
     """
-    check if a number is prime.
+    Check if a number is prime.
     """
     if n < 2:
         return False
     if n == 2:
         return True
-    for i in range(3, (n ** 1/2) + 1, 2):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
 
 def PrimeInRange(end):
     """
-    find all primes in the range (start-end)
+    Find all primes in the range (2 to end).
     """
-    return [n for n in range(2, end+1) if isPrime(n)]
+    return [n for n in range(2, end + 1) if isPrime(n)]
 
 def isWinner(x, nums):
     """
-    to find winner in the game.
+    Determine the winner of the game.
     """
     winsCountMaria, winsCountBen = 0, 0
-    for num in nums:
-        rounds = list(range(1, num + 1))
-        primes = PrimeInRange(num)
 
-        if primes is None:
+    for num in nums:
+        primes = PrimeInRange(num)
+        if not primes:
             winsCountBen += 1
             continue
-        
+
+        rounds = list(range(1, num + 1))
+        turn = 0  # 0 for Maria, 1 for Ben
+
+        while primes:
+            prime = primes.pop(0)
+            rounds = [n for n in rounds if n % prime != 0]
+            primes = [p for p in primes if p in rounds]
+            turn = 1 - turn
+
+        if turn == 0:
+            winsCountBen += 1
+        else:
+            winsCountMaria += 1
+
+    if winsCountMaria > winsCountBen:
+        return "Maria"
+    elif winsCountBen > winsCountMaria:
+        return "Ben"
+    else:
+        return None
