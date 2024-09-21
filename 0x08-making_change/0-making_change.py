fc2ed056738cff37848fdd5d2d66962cbabff84d
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-09-21 14:29:24 +0100

    0x08 - fixed #0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,14 +8,21 @@ def makeChange(coins, total):
     """
     Return: fewest number of coins needed to meet total
     """
+
+    if coins is None:
+        return -1
+
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    change = 0
+
+    coins = sorted(coins)[::-1]
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != total + 1 else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
